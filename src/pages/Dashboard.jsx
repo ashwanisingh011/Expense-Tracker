dd7fd7a055f61401e7880a50196bfa3299b55ac8
ashwanisingh011 <ashwanisingh9737@gmail.com>
2025-12-15 18:44:47 +0530

    Login Page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,9 @@
 /* eslint-disable react-refresh/only-export-components */
+// toast
+import { toast } from 'react-toastify';
+//Components
+import Intro from '../components/Intro';
+//
 import React from 'react'
 // helper functions
 import { fetchData } from '../helper'
@@ -9,12 +14,25 @@ export function dashboardLoader(){
     const userName = fetchData("userName");
     return {userName}
 }
+
+// actions
+export async function dashboardAction({request}){
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName))
+    return toast.success(`Welcome, ${formData.userName}`)
+  } catch (error) {
+    throw new error("There was a problem creating your account")
+  }
+}
+  
 const Dashboard = () => {
     const {userName} = useLoaderData()
   return (
     <div>
-        <h1>{userName}</h1>
-        Dashboard
+        {userName ? (<p>{userName}</p>) : (<Intro/>)}
+        
     </div>
   )
 }
