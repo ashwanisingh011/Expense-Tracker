65275021e88aee946b3fbc964b6d45960aa21a97
ashwanisingh011 <ashwanisingh9737@gmail.com>
2025-12-16 18:16:45 +0530

    Added Expense Form

--- src/helper.js
+++ src/helper.js
@@ -1,35 +1,82 @@
-export const wait = () => new Promise(res => setTimeout(res, Math.random() * 2000))
+export const waait = () => new Promise(res => setTimeout(res, Math.random() * 2000))
+
 
 // colors
 const generateRandomColor = () => {
-    const exisitingBudgetLength = fetchData("budgets") ?. length ?? 0;
-    return `${exisitingBudgetLength * 34} 65% 50%`
+  const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+  return `${existingBudgetLength * 34} 65% 50%`
 }
-
-
 
 // Local storage
 export const fetchData = (key) => {
-    return JSON.parse(localStorage.getItem(key));
+  return JSON.parse(localStorage.getItem(key));
 };
 
 // create budget
 export const createBudget = ({
-    name, amount
+  name, amount
 }) => {
-    const newItem = {
-        id: crypto.randomUUID(),
-        name: name,
-        createdAt: Date.now(),
-        amount: +amount,
-        color: generateRandomColor() 
-    }
-    const exisitingBudgets = fetchData("budgets") ?? []
-    return localStorage.setItem("budgets", JSON.stringify([...exisitingBudgets, newItem]))
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    color: generateRandomColor()
+  }
+  const existingBudgets = fetchData("budgets") ?? [];
+  return localStorage.setItem("budgets",
+    JSON.stringify([...existingBudgets, newItem]))
+}
+
+// create expense
+export const createExpense = ({
+  name, amount, budgetId
+}) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    budgetId: budgetId
+  }
+  const existingExpenses = fetchData("expenses") ?? [];
+  return localStorage.setItem("expenses",
+    JSON.stringify([...existingExpenses, newItem]))
+}
+
+// delete item
+export const deleteItem = ({ key }) => {
+  return localStorage.removeItem(key)
+}
+
+// total spent by budget
+export const calculateSpentByBudget = (budgetId) => {
+  const expenses = fetchData("expenses") ?? [];
+  const budgetSpent = expenses.reduce((acc, expense) => {
+    // check if expense.id === budgetId I passed in
+    if (expense.budgetId !== budgetId) return acc
+
+    // add the current amount to my total
+    return acc += expense.amount
+  }, 0)
+  return budgetSpent;
 }
 
 
-// delete item
-export const deleteItem = ({key})=>{
-    return localStorage.removeItem(key)
+// FORMATTING
+
+// Formating percentages
+export const formatPercentage = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: "percent",
+    minimumFractionDigits: 0,
+  })
+}
+
+// Format currency
+export const formatCurrency = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: "currency",
+    currency: "INR"
+  })
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,69 +1,107 @@
-/* eslint-disable react-refresh/only-export-components */
-// toast
-import { toast } from 'react-toastify';
-//Components
-import Intro from '../components/Intro';
-import AddBudgetForm from '../components/AddBudgetForm';
-//
-import React from 'react'
-// helper functions
-import { createBudget, fetchData, wait } from '../helper'
 // rrd imports
-import { useLoaderData } from 'react-router-dom';
-//loader
-export function dashboardLoader(){
-    const userName = fetchData("userName");
-    const budgets = fetchData("budgets");
-    return {userName, budgets}
+import { useLoaderData } from "react-router-dom";
+
+// library imports
+import { toast } from "react-toastify";
+
+// components
+import Intro from "../components/Intro";
+import AddBudgetForm from "../components/AddBudgetForm";
+import AddExpenseForm from "../components/AddExpenseForm";
+import BudgetItem from "../components/BudgetItem";
+
+//  helper functions
+import { createBudget, createExpense, fetchData, waait } from "../helper"
+
+// loader
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  const budgets = fetchData("budgets");
+  return { userName, budgets }
 }
 
-// actions
-export async function dashboardAction({request}){
-  await wait()
+// action
+export async function dashboardAction({ request }) {
+  await waait();
 
   const data = await request.formData();
-  const {_action, ...values} = Object.fromEntries(data);
-  
-  // new user sumbission
-  if(_action === "newUser"){
-      try {
-    localStorage.setItem("userName", JSON.stringify(values.userName))
-    return toast.success(`Welcome, ${values.userName}`)
-  } catch (error) {
-    throw new error("There was a problem creating your account")
+  const { _action, ...values } = Object.fromEntries(data)
+
+  // new user submission
+  if (_action === "newUser") {
+    try {
+      localStorage.setItem("userName", JSON.stringify(values.userName))
+      return toast.success(`Welcome, ${values.userName}`)
+    } catch (e) {
+      throw new Error("There was a problem creating your account.")
+    }
   }
-  }
-  if(_action === "createBudget"){
+
+  if (_action === "createBudget") {
     try {
       createBudget({
         name: values.newBudget,
-        amount: values.newBudgetAmount
+        amount: values.newBudgetAmount,
       })
       return toast.success("Budget created!")
-    } catch (error) {
-      throw new Error("There was a problem creating your budget")
+    } catch (e) {
+      throw new Error("There was a problem creating your budget.")
+    }
+  }
+
+  if (_action === "createExpense") {
+    try {
+      createExpense({
+        name: values.newExpense,
+        amount: values.newExpenseAmount,
+        budgetId: values.newExpenseBudget
+      })
+      return toast.success(`Expense ${values.newExpense} created!`)
+    } catch (e) {
+      throw new Error("There was a problem creating your expense.")
     }
   }
 }
-  
+
 const Dashboard = () => {
-    const {userName, budgets} = useLoaderData()
+  const { userName, budgets } = useLoaderData()
+
   return (
-    <div>
-        {userName ? (<div className='dashboard'>
-          <h1>Welcom back, <span className='accent'>{userName}</span></h1>
-          <div className="gird-sm">
-            {/* {budgets ? () : ()} */}
-            <div className='grid-lg'>
-              <div className="flex-lg">
-                <AddBudgetForm/>
-              </div>
-            </div>
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>Welcome back, <span className="accent">{userName}</span></h1>
+          <div className="grid-sm">
+            {
+              budgets && budgets.length > 0
+                ? (
+                  <div className="grid-lg">
+                    <div className="flex-lg">
+                      <AddBudgetForm />
+                      <AddExpenseForm budgets={budgets} />
+                    </div>
+                    <h2>Existing Budgets</h2>
+                    <div className="budgets">
+                      {
+                        budgets.map((budget) => (
+                          <BudgetItem key={budget.id} budget={budget} />
+                        ))
+                      }
+                    </div>
+                  </div>
+                )
+                : (
+                  <div className="grid-sm">
+                    <p>Personal budgeting is the secret to financial freedom.</p>
+                    <p>Create a budget to get started!</p>
+                    <AddBudgetForm />
+                  </div>
+                )
+            }
           </div>
-        </div>) : (<Intro/>)}
-        
-    </div>
+        </div>
+      ) : <Intro />}
+    </>
   )
 }
-
 export default Dashboard
