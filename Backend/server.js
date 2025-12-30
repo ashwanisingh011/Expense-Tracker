2ef32dba431def79345e970c470aa1b9fee706c4
ashwanisingh011 <ashwanisingh9737@gmail.com>
2025-12-31 00:44:38 +0530

    cors issue

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,9 @@ const app = express();
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://expense-trakker.netlify.app"]
+}));
 app.use(express.json());
 
 // Routes
